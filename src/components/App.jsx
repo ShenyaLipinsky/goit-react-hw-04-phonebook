a255e785bb262deef1e3499bf46d2cdfc887534e
ShenyaLipinsky <91908218+ShenyaLipinsky@users.noreply.github.com>
2022-07-15 16:43:55 +0300

    hard

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,48 +1,49 @@
-import { Component } from 'react';
 import { Box } from './Box';
 import { ContactForm } from './Phonebook/Phonebook';
 import { Contacts } from './Contacts/Contacts';
 import { Filter } from './Filter/Filter';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
-export class App extends Component {
-  state = {
-    contacts: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
+const LS_KEY = 'contacts';
+
+export const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    return JSON.parse(localStorage.getItem(LS_KEY)) ?? [];
+  });
+  const [filter, setFilter] = useState('');
+
+  console.log(contacts);
+  // const savedLocalData = localStorage.getItem(LS_KEY);
+  // const parsedLocalData = JSON.parse(localStorage.getItem(LS_KEY));
+
+  // useEffect(() => {
+  //   if (localStorage.getItem(LS_KEY) === null) {
+  //     return;
+  //   }
+  //   setContacts(() => { return JSON.parse(localStorage.getItem(LS_KEY)) ?? []});
+  // }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LS_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
+  // componentDidUpdate(prevState) {
+  //   if (prevState.contacts !== this.state.contacts || null) {
+  //     return localStorage.setItem(
+  //       LS_KEY,
+  //       JSON.stringify(this.state.contacts)
+  //     );
+  //   }
+  //   return;
+  // }
+
+  const handleChange = evt => {
+    setFilter(evt.target.value);
   };
 
-  componentDidMount() {
-    // const savedLocalData = localStorage.getItem('contacts');
-    // const parsedLocalData = JSON.parse(savedLocalData);
-    try {
-      const savedLocalData = localStorage.getItem('contacts');
-      const parsedLocalData =
-        savedLocalData === null ? [] : JSON.parse(savedLocalData);
-      return this.setState({ contacts: parsedLocalData });
-    } catch (error) {
-      console.error('Get state error: ', error.message);
-    }
-  }
-
-  componentDidUpdate(prevState) {
-    if (prevState.contacts !== this.state.contacts || null) {
-      return localStorage.setItem(
-        'contacts',
-        JSON.stringify(this.state.contacts)
-      );
-    }
-    return;
-  }
-
-  handleChange = evt => {
-    this.setState({ filter: evt.target.value });
-  };
-
-  filterItems = (arr, query) => {
+  const filterItems = (arr, query) => {
+    console.log(arr);
     if (arr.length !== 0 || null) {
       let newArray = arr.filter(
         el => el.name.toLowerCase().indexOf(query.toLowerCase()) !== -1
@@ -52,60 +53,160 @@ export class App extends Component {
     return;
   };
 
-  addContact = values => {
-    this.setState(prevState => {
+  const addContact = values => {
+    setContacts(contacts => {
       if (
-        prevState.contacts.find(
+        contacts.find(
           contact => contact.name.toLowerCase() === values.name.toLowerCase()
         )
       ) {
         return alert(`${values.name} is already in contacts.`);
-      } else {
-        return { contacts: [...prevState.contacts, values] };
       }
+      return [...contacts, values];
     });
   };
 
-  removeContact = id => {
-    this.setState(prevState => {
-      const newArray = prevState.contacts.filter(el => el.id !== id);
-      return { contacts: newArray };
+  const removeContact = id => {
+    setContacts(prevState => {
+      console.log(prevState);
+
+      return prevState.filter(el => el.id !== id);
     });
   };
+  return (
+    <Box
+      mt={4}
+      mx={'auto'}
+      px={4}
+      py={5}
+      display={'flex'}
+      flexDirection="column"
+      alignItems="center"
+      width={3}
+      borderRadius={'large'}
+      bg={'secondary'}
+    >
+      <h1>PhoneBook</h1>
+      <ContactForm onAddContact={addContact}></ContactForm>
+      {contacts.length > 0 && (
+        <>
+          <h2>Contacts</h2>
+          <Filter
+            title="Find contacts by name"
+            value={filter}
+            filterText={handleChange}
+          />
+          <Contacts
+            contacts={filterItems(contacts, filter)}
+            onRemoveContact={removeContact}
+          />
+        </>
+      )}
+    </Box>
+  );
+};
 
-  render() {
-    const { filter } = this.state;
-    return (
-      <Box
-        mt={4}
-        mx={'auto'}
-        px={4}
-        py={5}
-        display={'flex'}
-        flexDirection="column"
-        alignItems="center"
-        width={3}
-        borderRadius={'large'}
-        bg={'secondary'}
-      >
-        <h1>PhoneBook</h1>
-        <ContactForm onAddContact={this.addContact}></ContactForm>
-        {this.state.contacts.length > 0 && (
-          <>
-            <h2>Contacts</h2>
-            <Filter
-              title="Find contacts by name"
-              value={filter}
-              filterText={this.handleChange}
-            />
-            <Contacts
-              contacts={this.filterItems(this.state.contacts, filter)}
-              onRemoveContact={this.removeContact}
-            />
-          </>
-        )}
-      </Box>
-    );
-  }
-}
-// end
+// export class App extends Component {
+//   state = {
+//     contacts: [
+//       // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//       // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//       // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//       // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+//     ],
+//     filter: '',
+//   };
+
+//   componentDidMount() {
+//     // const savedLocalData = localStorage.getItem(LS_KEY);
+//     // const parsedLocalData = JSON.parse(savedLocalData);
+//     try {
+//       const savedLocalData = localStorage.getItem(LS_KEY);
+//       const parsedLocalData =
+//         savedLocalData === null ? [] : JSON.parse(savedLocalData);
+//       return this.setState({ contacts: parsedLocalData });
+//     } catch (error) {
+//       console.error('Get state error: ', error.message);
+//     }
+//   }
+
+//   componentDidUpdate(prevState) {
+//     if (prevState.contacts !== this.state.contacts || null) {
+//       return localStorage.setItem(
+//         LS_KEY,
+//         JSON.stringify(this.state.contacts)
+//       );
+//     }
+//     return;
+//   }
+
+//   handleChange = evt => {
+//     this.setState({ filter: evt.target.value });
+//   };
+
+//   filterItems = (arr, query) => {
+//     if (arr.length !== 0 || null) {
+//       let newArray = arr.filter(
+//         el => el.name.toLowerCase().indexOf(query.toLowerCase()) !== -1
+//       );
+//       return newArray;
+//     }
+//     return;
+//   };
+
+//   addContact = values => {
+//     this.setState(prevState => {
+//       if (
+//         prevState.contacts.find(
+//           contact => contact.name.toLowerCase() === values.name.toLowerCase()
+//         )
+//       ) {
+//         return alert(`${values.name} is already in contacts.`);
+//       } else {
+//         return { contacts: [...prevState.contacts, values] };
+//       }
+//     });
+//   };
+
+//   removeContact = id => {
+//     this.setState(prevState => {
+//       const newArray = prevState.contacts.filter(el => el.id !== id);
+//       return { contacts: newArray };
+//     });
+//   };
+
+//   render() {
+//     const { filter } = this.state;
+//     return (
+//       <Box
+//         mt={4}
+//         mx={'auto'}
+//         px={4}
+//         py={5}
+//         display={'flex'}
+//         flexDirection="column"
+//         alignItems="center"
+//         width={3}
+//         borderRadius={'large'}
+//         bg={'secondary'}
+//       >
+//         <h1>PhoneBook</h1>
+//         <ContactForm onAddContact={this.addContact}></ContactForm>
+//         {this.state.contacts.length > 0 && (
+//           <>
+//             <h2>Contacts</h2>
+//             <Filter
+//               title="Find contacts by name"
+//               value={filter}
+//               filterText={this.handleChange}
+//             />
+//             <Contacts
+//               contacts={this.filterItems(this.state.contacts, filter)}
+//               onRemoveContact={this.removeContact}
+//             />
+//           </>
+//         )}
+//       </Box>
+//     );
+//   }
+// }
--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -6,65 +6,68 @@ import {
   PhonebookBtn,
 } from './Phonebook.styled';
 import { nanoid } from 'nanoid';
-import { Component } from 'react';
+import { useState } from 'react';
 
-const INIT_VALUES = {
-  name: '',
-  number: '',
+export const ContactForm = ({ onAddContact }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+
+      default:
+        return;
+    }
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+    onAddContact({ name: name, number: number, id: nanoid(10) });
+    reset();
+  };
+
+  const reset = () => {
+    setName('');
+    setNumber('');
+  };
+
+  return (
+    <PhonebookForm autoComplete="off" onSubmit={handleSubmit}>
+      <PhonebookLabel htmlFor="name">
+        Name:
+        <PhonebookInput
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleChange}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </PhonebookLabel>
+      <PhonebookLabel htmlFor="number">
+        Phone:
+        <PhonebookInput
+          type="tel"
+          name="number"
+          value={number}
+          onChange={handleChange}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </PhonebookLabel>
+      <PhonebookBtn type="submit">Add Contact</PhonebookBtn>
+    </PhonebookForm>
+  );
 };
 
-export class ContactForm extends Component {
-  state = { ...INIT_VALUES };
-
-  static propTypes = {
-    onAddContact: propTypes.func.isRequired,
-  };
-
-  handleChange = e => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  };
-  handleSubmit = e => {
-    const { onAddContact } = this.props;
-    e.preventDefault();
-    onAddContact({ ...this.state, id: nanoid(10) });
-    this.reset();
-  };
-
-  reset = () => {
-    this.setState({ ...INIT_VALUES });
-  };
-
-  render() {
-    const { name, number } = this.state;
-    return (
-      <PhonebookForm autoComplete="off" onSubmit={this.handleSubmit}>
-        <PhonebookLabel htmlFor="name">
-          Name:
-          <PhonebookInput
-            type="text"
-            name="name"
-            value={name}
-            onChange={this.handleChange}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-        </PhonebookLabel>
-        <PhonebookLabel htmlFor="number">
-          Phone:
-          <PhonebookInput
-            type="tel"
-            name="number"
-            value={number}
-            onChange={this.handleChange}
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-          />
-        </PhonebookLabel>
-        <PhonebookBtn type="submit">Add Contact</PhonebookBtn>
-      </PhonebookForm>
-    );
-  }
-}
+ContactForm.propTypes = {
+  onAddContact: propTypes.func.isRequired,
+};
